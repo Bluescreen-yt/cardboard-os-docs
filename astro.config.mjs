0db8c912eca4b2990a44d0d0b430567b0cf1caa0
bluescreeen <Olivierus303@gmail.com>
2026-07-14 21:27:38 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,20 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig({ 
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Cardboard OS docs',
+			favicon:'./src/assets/logo.svg',
+			logo: {
+				src: './src/assets/logo.svg'
+			},
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/Bluescreen-yt/CardBoard-OS-CCT' },
+				{ icon: 'youtube', label: 'Youtube', href: 'https://www.youtube.com/@BlueScreenOffical88' },
+				{ icon: 'discord', label: 'Youtube', href: 'https://discord.gg/BHG7FBDdjs' }
+			],
+
 			sidebar: [
 				{
 					label: 'Guides',
@@ -18,7 +27,9 @@ export default defineConfig({
 				},
 				{
 					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
+					items: [
+						{ autogenerate: { directory: 'reference' } }
+					],
 				},
 			],
 		}),
